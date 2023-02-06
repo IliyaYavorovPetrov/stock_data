7c1b95ce9a722f45f21b74b128d37c9b74aff4fc
I.P <72142537+IliyaYavorovPetrov@users.noreply.github.com>
2023-02-06 13:30:33 +0200

    enable cashing

--- server/src/main/java/com/stock/server/ServerApplication.java
+++ server/src/main/java/com/stock/server/ServerApplication.java
@@ -2,12 +2,12 @@ package com.stock.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
